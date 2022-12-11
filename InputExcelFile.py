482bb0c0f9ba2fe56c7d6eef2db868734ce7f411
Michael <44344017+mtangtran@users.noreply.github.com>
2022-12-11 14:54:55 -0500

    Improved the comments.

--- InputExcelFile.py
+++ InputExcelFile.py
@@ -1,5 +1,9 @@
 """
+This class intends to be a resource for adding user input into the csv file.
 Add data to a csv file
+
+Resources:
+1. https://devenum.com/numpy-savetxt-to-append-to-existing-csv-file/
 """
 import numpy as np
 
@@ -8,4 +12,3 @@ class InputExcelFile:
     def __init__(self):
         self.csv_file = open("file.csv", "a")
 
-    
