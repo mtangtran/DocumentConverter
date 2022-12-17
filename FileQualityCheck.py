a78743a08307c8fe42efa8aaad4a2aa0ed9f3a8f
Michael <44344017+mtangtran@users.noreply.github.com>
2022-12-17 07:41:26 -0500

    File comparison handler file created. More functionality needs to be established.

--- FileQualityCheck.py
+++ FileQualityCheck.py
@@ -1,4 +1,5 @@
 import filecmp
+import os
 
 
 def file_comparison(file1, file2):
@@ -15,3 +16,30 @@ def file_comparison(file1, file2):
         return True
     else:
         return False
+
+
+def getFileSize(filename):
+    """
+    Get the size of the file.
+    :param filename:
+    :return:
+    """
+    os.path.getsize(filename)
+
+
+def getFileSizeComparison(file1, file2):
+    """
+    Compares the two inputted files and then returns a string on the relative size comparison.
+    :param file1:
+    :param file2:
+    :return:
+    """
+    file1_size = os.path.getsize(file1)
+    file2_size = os.path.getsize(file2)
+
+    if file1_size > file2_size:
+        return "{} is larger than {}".format(file1, file2)
+    elif file1_size == file2_size:
+        return "They are the same size"
+    elif file1_size < file2_size:
+        return "{} is smaller than {}".format(file1, file2)
